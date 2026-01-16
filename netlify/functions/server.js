b8e639a449ab1f0bee2b6fed681936b03ed11923
samuel Adeyinka <tboysammy@gmail.com>
2026-01-16 12:55:43 -0600

    Fix: simplify serverless function import and ensure backend dependencies are installed

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,6 +1,4 @@
 const serverless = require('serverless-http');
-// Import the app from backend/src/app.js
-// From /netlify/functions/server.js, go up 2 levels to root, then into /backend/src/app.js
-const app = require(require('path').resolve(__dirname, '../../backend/src/app'));
+const app = require('../../backend/src/app');
 
 module.exports.handler = serverless(app);
